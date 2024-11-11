879a42bfa2a46f5a36aff3aa464b9be94cf5a964
Vijai Kiren <vijaikirenofficial24@gmail.com>
2024-11-11 20:10:39 +0530

    feat(Imporoved Qr funtionality

--- src/Pages/TicketView.tsx
+++ src/Pages/TicketView.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QRCode from 'react-qr-code';
+// @ts-ignore
+import { db } from '../firebaseConfig';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-const Ticket: React.FC = () => { // Get the event ID from the URL
+const Ticket: React.FC = () => {
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+  
+
+  // Fetch user email if logged in
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user && user.email) {
+        setUserEmail(user.email); // Set the email if user is logged in
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="max-w-xs mx-auto bg-white rounded-lg shadow-lg justify-center items-center">
         {/* Top section with event image */}
         <div className="bg-[#246D8C] rounded-t-lg p-4 flex justify-center items-center">
-          
+          {/* Event Image or Logo */}
         </div>
 
         {/* Event Title */}
@@ -35,15 +52,14 @@ const Ticket: React.FC = () => { // Get the event ID from the URL
 
         {/* Placeholder for QR Code */}
         <div className="flex justify-center p-4">
-         <QRCode 
-           size={150}
-           value='text'
+          <QRCode 
+            size={150}
+            value={userEmail ? userEmail : 'No user logged in'} // Set QR code to user email
           />
-
         </div>
       </div>
     </div>
   );
 };
 
-export default Ticket
+export default Ticket;
